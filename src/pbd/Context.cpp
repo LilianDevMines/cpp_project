2ce279e42ff37044c97b806ac33d3bdeae7a40d3
Charsutty <charles.m.sutty@gmail.com>
2024-01-10 09:22:46 +0100

    ça maaaaarche (pbs sur la normal)

--- src/pbd/Context.cpp
+++ src/pbd/Context.cpp
@@ -33,8 +33,9 @@ void Context::addParticle(Vec2 pos, float radius, float mass, Vec2 velocity, int
 
 void Context::addPlan(Vec2 coord1, Vec2 coord2) {
     //Il faut ici ajouter un élément de la classe Particle au tableau m_particles dont le numéro est m_num_particles
-    Vec2 normal = Vec2{(coord1.x - coord2.x)/(abs(coord1.x) + abs(coord2.x)),
-              (coord2.y - coord1.y)/(abs(coord1.y) + abs(coord2.y))};
+    Vec2 directeur = Vec2{(coord2.x - coord1.x),(coord2.y - coord1.y)};
+    Vec2 normal = Vec2{(-directeur.y)/(std::sqrt(directeur.x*directeur.x + directeur.y*directeur.y)),
+                      (directeur.x)/(std::sqrt(directeur.x*directeur.x + directeur.y*directeur.y))};
     this->m_plans[m_num_plans++] = Plan{coord1, normal};
 }
 
@@ -74,8 +75,8 @@ void Context::dampVelocities()
 void Context::updateExpectedPosition(float dt)
 {
   for (int i =0;i <this->m_num_particles; i++){
-    this->m_particles[i].next_pos = Vec2{this->m_particles[i].next_pos.x + dt*this->m_particles[i].velocity.x,
-                                        this->m_particles[i].next_pos.y + dt*this->m_particles[i].velocity.y};
+    this->m_particles[i].next_pos = Vec2{this->m_particles[i].position.x + dt*this->m_particles[i].velocity.x,
+                                        this->m_particles[i].position.y + dt*this->m_particles[i].velocity.y};
   }
 }
 
@@ -86,11 +87,11 @@ void Context::addDynamicContactConstraints()
 void Context::addStaticContactConstraints(){
   for (int i =0;i <this->m_num_particles; i++){
     for (int p =0;p <this->m_num_plans; p++){
-      Vec2 proj_orig = Vec2{this->m_particles[i].next_pos.x - this->m_plans[p].point.x,
-                       this->m_particles[i].next_pos.y - this->m_plans[p].point.y}; //Calcule P - Q où P à projeter et Q dans le plan
+      Vec2 proj_orig = Vec2{this->m_particles[i].position.x - this->m_plans[p].point.x,
+                       this->m_particles[i].position.y - this->m_plans[p].point.y}; //Calcule P - Q où P à projeter et Q dans le plan
       Vec2 projection = Vec2{
-        this->m_particles[i].next_pos.x - produit_scalaire(proj_orig, this->m_plans[p].normal) * this->m_plans[p].normal.x,
-        this->m_particles[i].next_pos.y - produit_scalaire(proj_orig, this->m_plans[p].normal) * this->m_plans[p].normal.y};  
+        this->m_particles[i].position.x - produit_scalaire(proj_orig, this->m_plans[p].normal) * this->m_plans[p].normal.x,
+        this->m_particles[i].position.y - produit_scalaire(proj_orig, this->m_plans[p].normal) * this->m_plans[p].normal.y};  
         //Calcule la projection de P sur le plan
       if (produit_scalaire(Vec2{this->m_particles[i].next_pos.x - projection.x,
                                this->m_particles[i].next_pos.y - projection.y},this->m_plans[p].normal) < this->m_particles[i].radius){
